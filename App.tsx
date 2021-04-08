1c1ee8ef6608d540a43cf29f2710de9f59a80b59
Mattia Sanfilippo <imattolo@me.com>
2021-04-08 21:54:35 +0200

    style: prettier format

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the UI Kitten TypeScript template
- * https://github.com/akveo/react-native-ui-kitten
- *
- * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
- *
+ * WhoSings App
+ * @author Mattia Sanfilippo
  * @format
  */
 
 import React from 'react';
-import {
-  ImageProps,
-  StyleSheet,
-} from 'react-native';
+import {ImageProps, StyleSheet} from 'react-native';
 import {
   ApplicationProvider,
   Button,
@@ -23,29 +14,29 @@ import {
   Layout,
   Text,
 } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 
 /**
  * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
  * https://akveo.github.io/eva-icons
  */
-const HeartIcon = (props?: Partial<ImageProps>): React.ReactElement<ImageProps> => (
-  <Icon {...props} name='heart'/>
-);
+const HeartIcon = (
+  props?: Partial<ImageProps>,
+): React.ReactElement<ImageProps> => <Icon {...props} name="heart" />;
 
 export default (): React.ReactFragment => (
   <>
-    <IconRegistry icons={EvaIconsPack}/>
+    <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
       <Layout style={styles.container}>
-        <Text style={styles.text} category='h1'>
+        <Text style={styles.text} category="h1">
           Welcome to UI Kitten 😻
         </Text>
-        <Text style={styles.text} category='s1'>
+        <Text style={styles.text} category="s1">
           Start with editing App.js to configure your App
         </Text>
-        <Text style={styles.text} appearance='hint'>
+        <Text style={styles.text} appearance="hint">
           For example, try changing theme to Dark by using eva.dark
         </Text>
         <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
