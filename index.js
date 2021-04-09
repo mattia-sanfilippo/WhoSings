a82a01461a697b3750cea30f5010e697beb3fb84
Mattia Sanfilippo <imattolo@me.com>
2021-04-09 16:01:44 +0200

    chore: add reactotron

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * @format
- */
-
+if (__DEV__) {
+  import('./reactotron.config').then(() =>
+    console.log('Reactotron initialized'),
+  );
+}
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
