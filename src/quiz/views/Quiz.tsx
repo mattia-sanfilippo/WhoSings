385f71e1bc678c437cb10ace13bc1dd7ae1a3952
Mattia Sanfilippo <imattolo@me.com>
2021-04-11 01:04:41 +0200

    feat: add initial quiz view

--- src/quiz/views/Quiz.tsx
+++ src/quiz/views/Quiz.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
-import {Layout, Text} from '@ui-kitten/components';
+import React, {useCallback, useEffect} from 'react';
+import {Button, Layout, Text} from '@ui-kitten/components';
+import {styles} from './styles';
+import {RootState} from 'src/shared/store/configureStore';
+import {useDispatch, useSelector} from 'react-redux';
+import {getTracks} from '../quiz.actions';
+import {useNavigation} from '@react-navigation/core';
 
 export const Quiz = () => {
+  const {loading, failure} = useSelector((state: RootState) => state.quiz);
+
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    dispatch(getTracks());
+  }, [dispatch]);
+
+  const onPressButton = useCallback(() => {
+    navigation.navigate('QuizBegin');
+  }, [navigation]);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (failure) {
+    return <Text>There was an error</Text>;
+  }
+
   return (
-    <Layout>
+    <Layout level="3">
       <Layout level="2">
-        <Text>Quiz Page</Text>
+        <Text category="h1" style={styles.title}>
+          Who Sings?
+        </Text>
+        <Layout level="1" style={styles.quizContainer}>
+          <Button style={styles.startButton} onPress={onPressButton}>
+            START
+          </Button>
+        </Layout>
       </Layout>
     </Layout>
   );
---
+++ src/quiz/views/styles.ts
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  title: {
+    marginHorizontal: 8,
+    marginVertical: 16,
+  },
+  mainCard: {
+    margin: 8,
+    height: 128,
+    justifyContent: 'center',
+    borderRadius: 25,
+  },
+  cardContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  quizCard: {
+    margin: 8,
+  },
+  quizContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  timer: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    padding: 8,
+  },
+  startButton: {
+    margin: 10,
+  },
+});
