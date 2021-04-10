835d1dbcbebba11e2ef4a1f537e250d13ae3d09f
Mattia Sanfilippo <imattolo@me.com>
2021-04-11 01:02:24 +0200

    feat: create util for shuffle and pick elements

---
+++ src/shared/utils/pickShuffled.ts
@@ -0,0 +1,16 @@
+export const pickShuffled = (elements: Array<any>, picks: number) => {
+  let currentIndex = elements.length,
+    temporaryValue,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = elements[currentIndex];
+    elements[currentIndex] = elements[randomIndex];
+    elements[randomIndex] = temporaryValue;
+  }
+
+  return elements.slice(0, picks);
+};
