2152ba9d4fbde5b7b4d7c6971d34c6269ebe9c83
Mattia Sanfilippo <imattolo@me.com>
2021-04-12 15:14:56 +0200

    feat: user leaderboard actions

--- src/leaderboard/leaderboard.actions.ts
+++ src/leaderboard/leaderboard.actions.ts
@@ -1,16 +1,43 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {GET_LEADERBOARD} from './leaderboard.constants';
+import {GET_LEADERBOARD, GET_USER_LEADERBOARD} from './leaderboard.constants';
+import {Rank} from './leaderboard.types';
 
 export const getLeaderboard = () => {
   return function (dispatch: any) {
     return AsyncStorage.getItem('leaderboard')
       .then((result) => {
-        result != null
-          ? dispatch(setLeaderboard(JSON.parse(result)))
-          : dispatch(setLeaderboard([]));
+        if (result != null) {
+          const ranks = JSON.parse(result);
+          dispatch(setLeaderboard(ranks));
+        } else {
+          dispatch(setLeaderboard([]));
+        }
       })
       .catch((error) => {
+        console.log(error);
         dispatch(setLeaderboard([], error));
+      });
+  };
+};
+
+export const getUserLeaderboard = (user: string) => {
+  return function (dispatch: any) {
+    return AsyncStorage.getItem('leaderboard')
+      .then((result) => {
+        if (result != null) {
+          const ranks = JSON.parse(result);
+          dispatch(
+            setUserLeaderboard(
+              ranks.filter((r: Rank) => r.playerName === user),
+            ),
+          );
+        } else {
+          dispatch(setUserLeaderboard([]));
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(setUserLeaderboard([], error));
       });
   };
 };
@@ -18,6 +45,14 @@ export const getLeaderboard = () => {
 const setLeaderboard = (leaderboard: any, error?: Error) => {
   return {
     type: GET_LEADERBOARD,
+    payload: leaderboard,
+    failure: error,
+  };
+};
+
+const setUserLeaderboard = (leaderboard: any, error?: Error) => {
+  return {
+    type: GET_USER_LEADERBOARD,
     payload: leaderboard,
     failure: error,
   };
--- src/leaderboard/reducer/leaderboard.reducer.ts
+++ src/leaderboard/reducer/leaderboard.reducer.ts
@@ -1,13 +1,18 @@
 import {Leaderboard} from '../leaderboard.types';
-import {GET_LEADERBOARD} from './../leaderboard.constants';
+import {
+  GET_LEADERBOARD,
+  GET_USER_LEADERBOARD,
+} from './../leaderboard.constants';
 
 export type LeaderboardState = {
+  userLeaderboard: Leaderboard;
   leaderboard: Leaderboard;
   loading: boolean;
   failure?: Error;
 };
 
 const initialState: LeaderboardState = {
+  userLeaderboard: [],
   leaderboard: [],
   loading: true,
   failure: undefined,
@@ -30,6 +35,13 @@ export const leaderboardReducer = (
         loading: false,
         failure: action.failure,
         leaderboard: sortLeaderboard(action.payload),
+      };
+    case GET_USER_LEADERBOARD:
+      return {
+        ...state,
+        loading: false,
+        failure: action.failure,
+        userLeaderboard: sortLeaderboard(action.payload),
       };
     default:
       return {
