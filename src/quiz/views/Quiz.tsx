b27d2fcf12cd7477c69e343df03f0c9f20157a1d
Mattia Sanfilippo <imattolo@me.com>
2021-04-12 15:16:51 +0200

    feat: add login

--- src/quiz/views/Quiz.tsx
+++ src/quiz/views/Quiz.tsx
@@ -1,13 +1,19 @@
 import React, {FC, useCallback, useEffect, useState} from 'react';
-import {Button, Input, Layout, Text} from '@ui-kitten/components';
+import {Button, Input, Layout, Spinner, Text} from '@ui-kitten/components';
 import {styles} from './styles';
 import {RootState} from 'src/shared/store/configureStore';
 import {useDispatch, useSelector} from 'react-redux';
 import {getTracks} from '../quiz.actions';
 import {useNavigation} from '@react-navigation/core';
+import {getUser, logInUser} from '../../user/user.actions';
+import Title from '../../components/Title';
 
 export const Quiz: FC = () => {
   const {loading, failure} = useSelector((state: RootState) => state.quiz);
+  const {user, isLoggedIn, loadingUser, failureUser} = useSelector(
+    (state: RootState) => state.user,
+  );
+
   const [name, setName] = useState('');
 
   const dispatch = useDispatch();
@@ -15,40 +21,43 @@ export const Quiz: FC = () => {
 
   useEffect(() => {
     dispatch(getTracks());
+    dispatch(getUser());
   }, [dispatch]);
 
   const onPressButton = useCallback(() => {
-    navigation.navigate('QuizBegin', {
-      nickname: name,
-    });
-  }, [name, navigation]);
+    !isLoggedIn
+      ? dispatch(logInUser(name, navigation, 'QuizBegin'))
+      : navigation.navigate('QuizBegin', {
+          nickname: user,
+        });
+  }, [dispatch, isLoggedIn, name, navigation, user]);
 
-  if (loading) {
-    return <Text>Loading...</Text>;
+  if (loading || loadingUser) {
+    return <Spinner />;
   }
 
-  if (failure) {
+  if (failure || failureUser) {
     return <Text>There was an error</Text>;
   }
 
   return (
     <Layout level="3">
       <Layout level="2">
-        <Text category="h1" style={styles.title}>
-          Who Sings?
-        </Text>
+        <Title title="Who Sings?" />
         <Layout level="1" style={styles.quizContainer}>
-          <Input
-            style={styles.startButton}
-            value={name}
-            label="Username"
-            placeholder="Type your username"
-            onChangeText={(nextName) => setName(nextName)}
-          />
+          {!isLoggedIn && (
+            <Input
+              style={styles.startButton}
+              value={name}
+              label="Username"
+              placeholder="Type your username"
+              onChangeText={(nextName) => setName(nextName)}
+            />
+          )}
           <Button
             style={styles.startButton}
             onPress={onPressButton}
-            disabled={name === ''}>
+            disabled={name === '' && user === ''}>
             START
           </Button>
         </Layout>
